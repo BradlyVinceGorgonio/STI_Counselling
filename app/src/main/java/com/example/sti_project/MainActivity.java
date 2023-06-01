afd70a6f7e5936e56a073b4071c2bc3fd781656c
DanielGals <janjangallaron@gmail.com>
2023-06-02 01:07:57 +0800

    Added prints and commentsas

--- app/src/main/java/com/example/sti_project/MainActivity.java
+++ app/src/main/java/com/example/sti_project/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("Tae mabaho");
         System.out.println("ASDASDASDASd");
+        System.out.println("asdasd");
         //asdasdasdasda
     }
 }
