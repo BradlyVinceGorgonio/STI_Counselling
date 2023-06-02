c7fe4af9825b6023148f69c2f71862ef00c456c1
DanielGals <janjangallaron@gmail.com>
2023-06-03 01:48:07 +0800

    Better Register UI, Added email, Pass and repass

--- app/src/main/java/com/example/sti_project/registerBtn.java
+++ app/src/main/java/com/example/sti_project/registerBtn.java
@@ -3,12 +3,25 @@ package com.example.sti_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class registerBtn extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class registerBtn extends AppCompatActivity
+{
+    private FirebaseAuth mAuth;
+
+    EditText name, email, pass, repass;
+    Button registerBtn, alreadyBtn;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_btn);
+
+
     }
 }
