4d09e39406ba26922934bc8115f16dfd288aa019
BradlyVinceGorgonio <118922196+BradlyVinceGorgonio@users.noreply.github.com>
2023-06-02 22:57:04 +0800

    STI_PROJECT

---
+++ app/src/main/java/com/example/sti_project/LoginBtn.java
@@ -0,0 +1,15 @@
+package com.example.sti_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginBtn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_btn);
+    }
+
+}
